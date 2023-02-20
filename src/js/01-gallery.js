284eb363e7153d01ee75e6b4a071a4bb104b23d1
MisuykMaryna <misyuk.marina@gmail.com>
2023-02-20 19:58:38 +0100

    hw4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryImages = (galleryItems) => 
+    galleryItems.map(({ preview, original, description }) => 
+   `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`).join('');
+ 
+galleryEl.insertAdjacentHTML("beforeend", galleryImages(galleryItems));
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: "alt",
+ });
+
+
+console.log(galleryImages(galleryItems));
+
