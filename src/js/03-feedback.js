4a0dbda79d145846099d0641c1241c327af6765f
MisuykMaryna <misyuk.marina@gmail.com>
2023-02-21 21:11:56 +0100

    hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,19 +11,23 @@ formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem("STORAGE_KEY", message);
+   formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
  
 function onFormSubmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
-    localStorage.removeItem("STORAGE_KEY");
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
 }
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem("STORAGE_KEY");
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
     if (savedMessage) {
-        formEl.value = savedMessage; 
+        email.value = savedMessage.email;
+        message.value = savedMessage.message;
     }
 }
